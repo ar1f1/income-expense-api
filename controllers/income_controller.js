b911d3f3de1b7a3b6c3d4d4853635f16a6964d28
Ar1f1 <arifiabdulhaq@gmail.com>
2022-06-14 03:19:21 +0430

    CRUD methos for income

--- controllers/income_controller.js
+++ controllers/income_controller.js
@@ -1,69 +1,38 @@
-var Income = require('../model/income')
+const Income = require('../model/income')
 
 
-// Add an income
-const add_income = (req, res) => {
- var Income = new Income(req.body)
- income ._id = req.body.id
- income .amount = req.body.amount
- income .reason = req.body.reason
+module.exports.add_income = async(req, res) => {
+    try {
+        const { amount, reason } = req.body;
+        const income = await Income.create({ amount, reason })
+        res.status(200).json({ income: income._id })
 
- income .save((err, saveIncome) => {
-   if(err){
-     res.status(500).send({err:"Could not save product"})
-   }
-   else {
-     res.status(200).send(saveIncome)
-   }
- })
-}
-
-// Get all incomes
-const get_all_income = (req, res) => {
-  Income.find({}, (err, income) => {
-    if(err){
-      res.status(500).send({error:"could not fetch products"})
-    }else{
-      res.status(200).send(income)
+    } catch (error) {
+        res.status(400).json({ Error: "plase try agin" })
     }
-  })
-}
 
-// Delete an income
-const delete_one_income = (req, res) => {
- Income.deleteOne({_id:req.params.id}, (err, income) => {
-   if(err){
-     res.status(500).send({error:"Could not find the object"})
-   }
-   else{
-     res.status(200).send(income)
-   }
- })
 }
-
-const update_income = (req, res) => {
- const {amount, reason} = req.body
- Income.find({_id:req.params.id}, (err, income) => {
-   if(err){
-     res.status(500).send({eroor:"Document not found"})
-   }else{
-     var myquery = { _id:req.params.id };
-     var newvalues = {$set: {amount: amount, reason:reason} };
-     Income.updateOne(myquery, newvalues, (err, income) => {
-       if (err){
-         res.status(500).send({error:"Could not updated"})
-       }
-       else{
-         res.status(200).send(income)
-       }
-     })
-   }
- })
+module.exports.get_income = async(res, res) => {
+    try {
+        const income = await Income.findById({ id })
+        res.status(200).json({ income })
+    } catch (error) {
+        res.status(400).json({ Error: "please check the url paramiters(id)" })
+    }
 }
+module.exports.edit_income = async(req, res) => {
+    try {
+        const { amount, reason } = req.body
+        const income = await Income.findOneAndReplace({ id: _id }, {
+            $set: {
+                amount: amount,
+                reason: reason
+            }
+        })
+        res.status(200).json({ income: income._id })
 
-module.exports = {
-  add_income,
-  get_all_income,
-  delete_one_income,
-  update_income
-};
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
+}
